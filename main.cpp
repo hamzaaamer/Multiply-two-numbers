bfb77e10d419c4ab67941777eb207621a26b6294
Hamza Aamer <50561910+hamzaaamer@users.noreply.github.com>
2023-04-19 15:36:07 +0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+
+{
+    int a;
+    
+    int b;
+    
+    cout<<" MULTIPLY TWO NUMBERS.\n ";
+    
+    cout<<"\n Enter first value: ";
+    
+    cin >>a ;
+    
+    cout<<" \n Enter second value: ";
+    
+    cin >>b;
+    
+    cout<<" \n Result = ";
+
+    cout<<a*b;
+    
+}
+
+
+
